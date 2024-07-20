7eefec2e7e45abcac422d8e332d74f9534cb2545
fresh4 <fresh1736@gmail.com>
2024-07-20 15:05:51 -0500

    Swapped from using resource to prefab exports

--- Scripts/pepper.gd
+++ Scripts/pepper.gd
@@ -1,13 +1,21 @@
-extends Node
+@tool
+extends RigidBody2D
 class_name Pepper
 
-@export var properties: PepperTemplate
+@export_range(0, 15000000) var max_scoville: int = 1000 ## Maximum potential Scoville value for the pepper
+@export_range(0, 100) var required_light: int = 100 ## Optimal light levels for this pepper's growth
+@export var recipe: Array[PepperTemplate] ## What peppers combined can create this pepper; empty if base pepper
+@export var tooltip_text: String = "" ## Description to show in tooltip
+@export var is_pepper: bool = true ## Is this object a pepper? No for generic draggable physics objects
+@export var sprite_texture: Texture2D : set = editor_set_sprite ## Texture to use for this pepper
 
 @onready var tooltip: Label = %Tooltip
 @onready var sprite: Sprite2D = %Icon
 
 func _ready() -> void:
-	tooltip.text = properties.tooltip_text
-	if properties.sprite:
-		sprite.texture = properties.sprite
+	tooltip.text = tooltip_text
+	if sprite_texture:
+		sprite.texture = sprite_texture
 
+func editor_set_sprite(value: Texture2D) -> void:
+	sprite.texture = value
