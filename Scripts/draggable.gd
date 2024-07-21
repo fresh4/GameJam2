bb0778f85a5046d2d7b90f0ef257332000af0182
fresh4 <fresh1736@gmail.com>
2024-07-21 00:28:41 -0500

    Added non-pepper draggables and tweaks to draggable ogic

--- Scripts/draggable.gd
+++ Scripts/draggable.gd
@@ -1,18 +1,18 @@
-extends Node2D
+extends Area2D
 class_name DraggableObject
-
-@onready var area: Area2D = %DetectionArea
 
 var parent: RigidBody2D
 var hovered: bool = false
 var dragging: bool = false
 
 func _ready() -> void:
+	self.mouse_entered.connect(_on_mouse_entered)
+	self.mouse_exited.connect(_on_mouse_exited)
 	parent = get_parent() as RigidBody2D
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if dragging:
-		parent.apply_central_force((parent.get_viewport().get_mouse_position() - parent.get_position()) * 50 - parent.linear_velocity*6)
+		parent.apply_central_force((parent.get_viewport().get_mouse_position() - parent.get_position()) * 100 - parent.linear_velocity*12)
 		# I don't actually know a damn thing about how the above works
 
 func _input(_event: InputEvent) -> void:
@@ -21,8 +21,8 @@ func _input(_event: InputEvent) -> void:
 	elif Input.is_action_just_released("click"): 
 		dragging = false
 
-func _on_area_2d_mouse_entered() -> void:
+func _on_mouse_entered() -> void:
 	hovered = true
 
-func _on_area_2d_mouse_exited() -> void:
+func _on_mouse_exited() -> void:
 	hovered = false
