baeff5c50bb1bdd2747668429cbaaede60f5363a
fresh4 <fresh1736@gmail.com>
2024-07-24 07:43:28 -0500

    Animated bell

--- Scripts/bell.gd
+++ Scripts/bell.gd
@@ -4,14 +4,19 @@ extends Node2D
 @onready var bell_middle: Sprite2D = $BellMiddle
 @onready var bell_bottom: Sprite2D = $BellBottom
 @onready var bell_area: Area2D = $BellArea
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var hovering: bool = false
 
 func ring() -> void:
-	Globals.game_manager.market.summon()
+	if not Globals.game_manager.market.summoned:
+		Globals.game_manager.market.summon()
+	else: 
+		Globals.game_manager.market.unsummon()
+	animation_player.play("ring")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action("click") and hovering:
+	if event.is_action_pressed("click") and hovering:
 		ring()
 
 func _on_bell_area_mouse_entered() -> void:
